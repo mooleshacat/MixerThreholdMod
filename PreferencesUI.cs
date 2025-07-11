37e287d73b9d5b428b0651ca77a064263c63a206
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2025-07-10 20:10:40 -0400

    Initial commit

---
+++ PreferencesUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using Il2CppInterop.Runtime.Attributes;
+
+namespace MixerThreholdMod_0_0_1
+{
+    [RegisterTypeInIl2Cpp]
+    public class PreferencesUI : Il2CppUnityEngine.MonoBehaviour
+    {
+        public PreferencesUI(IntPtr ptr) : base(ptr) { }
+
+        public void Start()
+        {
+            // This class helps expose your mod's preferences to MelonPreferencesManager
+        }
+    }
+}
